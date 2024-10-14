7983aa781725c171da5a4aa50ba5e2d8e1a88426
Isangel gonzalez <isangelnik@gmail.com>
2024-10-14 10:27:32 -0400

    iniciando

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+import { montserrat } from './ui/fonts';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${montserrat.className} antialias`}>
+        {children}
+        </body>
+    </html>
+  );
+}
